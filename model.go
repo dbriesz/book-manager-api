90452093c7485c8e643d5331f131119f9f74608f
David Riesz <dbriesz@users.noreply.github.com>
2019-06-07 10:42:43 -0700

    Create book model struct, CRUD functions and getter for a book list

---
+++ model.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type book struct {
+	Title     string `json:"title"`
+	Author    string `json:"author"`
+	Publisher string `json:"publisher"`
+	Date      string `json:"date"`
+	Rating    int    `json:"rating"`
+	Status    string `json:"status"`
+}
+
+func (b *book) getBook(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (b *book) updateBook(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (b *book) deleteBook(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (b *book) createBook(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func getBooks(db *sql.DB, start, count int) ([]book, error) {
+	return nil, errors.New("Not implemented")
+}
